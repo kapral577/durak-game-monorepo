912a2673168702332b85f15c29b7059e8c4d9b0f
kapral577 <langaev199828@gmail.com>
2025-05-22 21:22:50 +0300

    🚀 fix: обёртка <BrowserRouter> добавлена для поддержки useNavigate

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -8,6 +9,8 @@ import './assets/grayscale.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
