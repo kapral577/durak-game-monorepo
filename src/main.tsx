0b7a72cde3aa0d1b46154bcce67b5e78ba9e1487
kapral577 <langaev199828@gmail.com>
2025-05-22 21:26:54 +0300

    ✅ fix: добавлен GameEngineProvider для поддержки useGame

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { GameEngineProvider } from './context/GameEngineProvider';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -10,7 +11,9 @@ import './assets/grayscale.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GameEngineProvider>
+        <App />
+      </GameEngineProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
