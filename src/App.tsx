f288dcb7820801bde6edb3f098a7a6eba4ebef2e
kapral577 <langaev199828@gmail.com>
2025-05-22 22:24:37 +0300

    ♻️ fix: удалён дублирующий GameEngineProvider из App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,22 @@ import TablesPage from './pages/TablesPage';
 import FriendsPage from './pages/FriendsPage';
 import GameRoomPage from './pages/GameRoomPage';
 import GamePlayPage from './pages/GamePlayPage';
-import { GameEngineProvider } from './context/GameEngineProvider';
 import { GameSettingsProvider } from './context/GameSettingsContext';
 import { WebSocketProvider } from './context/WebSocketProvider';
 
 const App: React.FC = () => {
   return (
     <WebSocketProvider>
-      <GameEngineProvider>
-        <GameSettingsProvider>
-          <Routes>
-            <Route path="/" element={<MainMenu />} />
-            <Route path="/setup" element={<GameSetupPage />} />
-            <Route path="/tables" element={<TablesPage />} />
-            <Route path="/friends" element={<FriendsPage />} />
-            <Route path="/room/:roomId" element={<GameRoomPage />} />
-            <Route path="/play" element={<GamePlayPage />} />
-          </Routes>
-        </GameSettingsProvider>
-      </GameEngineProvider>
+      <GameSettingsProvider>
+        <Routes>
+          <Route path="/" element={<MainMenu />} />
+          <Route path="/setup" element={<GameSetupPage />} />
+          <Route path="/tables" element={<TablesPage />} />
+          <Route path="/friends" element={<FriendsPage />} />
+          <Route path="/room/:roomId" element={<GameRoomPage />} />
+          <Route path="/play" element={<GamePlayPage />} />
+        </Routes>
+      </GameSettingsProvider>
     </WebSocketProvider>
   );
 };
