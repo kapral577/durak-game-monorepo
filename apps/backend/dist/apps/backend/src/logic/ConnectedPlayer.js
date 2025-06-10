e0b6888a9c0242b335ac0aa0bad3f4d213e2c674
kapral577 <langaev199828@gmail.com>
2025-06-11 00:32:23 +0300

    Update server code

--- apps/backend/dist/apps/backend/src/logic/ConnectedPlayer.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// durak-server/types/ConnectedPlayer.ts - РЕФАКТОРИРОВАННАЯ ВЕРСИЯ
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ConnectedPlayer.js.map
--- apps/backend/dist/apps/backend/src/types/GameState.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// durak-server/types/GameState.ts - ТОЛЬКО ИГРОВЫЕ ТИПЫ
-Object.defineProperty(exports, "__esModule", { value: true });
-// ✅ ВСЁ ОСТАЛЬНОЕ УДАЛЕНО - ИСПОЛЬЗУЕМ SHARED ТИПЫ
-//# sourceMappingURL=GameState.js.map
